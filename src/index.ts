7864426062ce65557ae9a3174646a28efa908885
piet <42157469+camper0008@users.noreply.github.com>
2021-04-27 18:29:53 +0200

    init commit index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import { hostname, post } from "./ajax";
+import { htmlElements } from "./dom";
+import { useUserHandler } from "./userHandler";
 import { displayForm } from "./popupHandler";
+import { useRoomHandler } from "./roomHandler";
 
-displayForm('login')
+const start = () => {
+    if (sessionStorage.getItem('token')) {
+        post(hostname + '/api/users/checktoken', {token: sessionStorage.getItem('token')}, (response: any) => {
+            if (!(response.success === true && response.response === 'success')) {
+                displayForm('login', response.response === 'expired' ? 'session expired' : undefined);
+            } else {
+                useRoomHandler();
+                useUserHandler();
+                htmlElements.popup.className = 'hidden';
+            }
+        })
+    } else {
+        displayForm('login')
+    }
+}
+
+start();
