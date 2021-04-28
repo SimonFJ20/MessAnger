1b09c4e450fdaaeafdee84f7db9bc21d39e79556
Simon From <simonfromjakobsen@gmail.com>
2021-04-28 21:55:24 +0200

    Update ajax.ts

--- src/ajax.ts
+++ src/ajax.ts
@@ -7,7 +7,7 @@
  * @param {object} data - Optional, parse a JavaScript object as requst body
  */
 
- export const hostname = 'http://172.16.115.168:80'
+ export const hostname = 'https://www.simonfj20.site'
 
  export const get = (url: string, callback: (response: object, error?: boolean) => void, data?: object): void => {
     
