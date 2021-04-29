294299ce0adf23f390a09dbdbf19833bd03c9dd2
Simon From <simonfromjakobsen@gmail.com>
2021-04-29 11:10:02 +0200

    Update messages.ts

--- src/api/messages.ts
+++ src/api/messages.ts
@@ -161,7 +161,7 @@ const setMessagsCheckupdated = (router: Router, database: Db, route: string) => 
             res.status(200).json({
                 success: true,
                 response: 'success',
-                lastUpdated: new Date(room.lastUpdated)
+                lastUpdated: new Date(room.lastUpdated).returnMillis()
             });
         } catch(error) {
             res.status(500).json({success: false, response: 'error'});
